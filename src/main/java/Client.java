47c33ef5d26ee33dab32169dc4ca5c23d15bc973
Aldo-rodal <92940614+Aldo-rodal@users.noreply.github.com>
2022-10-25 14:40:08 -0400

    Nuevo Java Gradle

---
+++ src/main/java/Client.java
@@ -0,0 +1,14 @@
+public class Client {
+    public static void main(String []asdasda) {
+        int a = 9;
+        int b = 8;
+
+        if(a > b) {
+            a = a * 2;
+            System.out.println(a);
+        } else {
+            b = b - 2;
+            System.out.println(b);
+        }
+    }
+}
